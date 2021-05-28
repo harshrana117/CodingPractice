cdfdcc31a52862578888b044ead6903d963773cd
Harsh Rana <harshrana117@gmail.com>
2021-05-28 11:24:15 +0530

    Left rotate by 1

---
+++ Arrays/Left rotate by one.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// //Left rotate(counter clockwise) an array by one
+
+// // i/p {1,2,3,4,5}
+// // o/p {2,3,4,5,1}
+
+// void lRotateOne(int arr[], int n){
+// int temp = arr[0];
+// for(int i=1; i<n; i++){
+//   arr[i-1] = arr[i];
+// }
+// arr[n-1] = temp;
+// }
+
+// int main() {
+//   int arr[] = { 1,2,3,4,5}, n=5 ;
+//   cout<<"Before Removal"<<endl;
+//        for(int i = 0; i < n; i++)
+//        {
+//        		cout<<arr[i]<<" ";
+//        }
+//        cout<<endl;
+//        lRotateOne(arr, n);
+//        cout<<"After Removal"<<endl;
+//        for(int i = 0; i < n; i++)
+//        {
+//        		cout<<arr[i]<<" ";
+//        }
+// }
