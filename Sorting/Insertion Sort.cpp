429c291aaa97cd3ba25457fd538e1230ca40aae1
Harsh Rana <harshrana117@gmail.com>
2021-07-07 10:09:52 +0530

    Insertion Sort

---
+++ Sorting/Insertion Sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Insertion Sort: O(n^2) worst case, Stable algorithm, O(n) best case
+
+void insertionSort(int arr[], int n){
+  for(int i=0; i<n; i++){
+    int key = arr[i];
+    int j = i-1;
+    //Condition for going into while loop
+    while(arr[j]> arr[i] && j>=0){
+      arr[j+1] = arr[j]; //Shifting elements
+      j--;
+    }
+    arr[j+1] = key;
+  }
+}
+
+
+
+
+int main() {
+  int arr[] = {4,2,7,4,9,10,2}, n=7;
+  int arr2[] = {3,4,5,6,1,2,3,4,8,9,10}, n2 = 11;
+  insertionSort( arr, n);
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  insertionSort(arr2, n2);
+  for(int i=0; i<n2; i++){
+    cout<<arr2[i]<<" ";
+  }
+  return 0;
+}
