3ba952661419262aea7970e6b056bccb96337f84
Harsh Rana <harshrana117@gmail.com>
2021-11-07 00:39:02 +0530

    Median in stream of arrays

---
+++ Heap/Median in stream of arrays.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+void printMedians(int arr[],int n){
+    priority_queue<int> s;
+    priority_queue<int, vector<int>,greater<int>> g;
+    s.push(arr[0]);
+    cout<<arr[0]<<" ";
+    for(int i=1;i<n;i++){
+        int x=arr[i];
+        if(s.size()>g.size())
+        {
+            if(s.top()>x){
+                g.push(s.top());
+                s.pop();
+                s.push(x);
+            }else g.push(x);
+            cout<<(s.top()+g.top())/2.0<<" ";
+        }else{
+            if(x<=s.top()){
+                s.push(x);
+            }else{
+                g.push(x);
+                s.push(g.top());
+                g.pop();
+            }
+            cout<<s.top()<<" ";
+        }
+    }
+}
+
+int main()
+{
+	int keys[] = { 12, 15, 10, 5, 8, 7, 16};
+    
+    printMedians(keys,7);
+	
+	return 0;
+}
+
+//Median of array stream
+
+//We maintain two containers of 1st half and 2nd half of elements
+
+//smaller half is priority queue of minHeap and last half is priority queue of MaxHeap
+
+//In case of even elements: median = avg of middle two elements
