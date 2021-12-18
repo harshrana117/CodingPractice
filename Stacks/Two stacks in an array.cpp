940a1e904905e7956739cc41ac67c4235ac24810
Harsh Rana <harshrana117@gmail.com>
2021-12-18 10:43:28 +0530

    Two stacks in an array

---
+++ Stacks/Two stacks in an array.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+struct TwoStacks{
+  int* arr;
+  int cap;
+  int top1, top2;
+
+  TwoStacks(int n){
+    cap = n;
+    arr = new int[n];
+    top1 = -1;
+    top2 = cap;
+  }
+
+  void push1(int x){
+    if(top1<top2-1){
+      top1++;
+      arr[top1]=x;
+    } else{
+      cout<<"Stack Overflow";
+      exit(1);
+    }
+  }
+
+  void push2(int x){
+    if(top1<top2-1){
+      top2--;
+      arr[top2] = x;
+    } else {
+      cout<<"Stack Overflow";
+      exit(1);
+    }
+  }
+
+  int pop1(){
+    if(top1 >= 0){
+      int x = arr[top1];
+      top1--;
+      return x;
+    } else {
+      cout<<"Stack Underflow";
+      exit(1);
+    }
+  }
+
+  int pop2(){
+    if(top2 < cap){
+      int x = arr[top2];
+      top2++;
+      return x;
+    } else {
+      cout<<"Stack Underflow";
+      exit(1);
+    }
+  }
+};
+
+
+
+
+int main(){
+
+  TwoStacks ts(5);
+  ts.push1(5);
+  ts.push2(10);
+  ts.push2(15);
+  ts.push1(11);
+  ts.push2(7); 
+  cout<<"Popped element from stack1 is "<<ts.pop1();
+  cout<<endl;
+  ts.push2(35);
+  cout<<"Popped element from stack1 is "<<ts.pop2();
+  return 0;
+
+}
